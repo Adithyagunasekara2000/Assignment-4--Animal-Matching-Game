f3857cc96d972fe81af334c3e263da60df77c88a
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-23 09:22:41 +0000

    body

---
+++ animal_matching_game/src/components/Body.js
@@ -0,0 +1,14 @@
+import './style.css'
+export default function Body(){
+    return (
+        <>
+        <table border="1">
+            <tr>
+                <td>Result</td>
+                <td>Animal Name</td>
+                <td>Select the Animal</td>
+            </tr>
+        </table>
+        </>
+    );
+}
